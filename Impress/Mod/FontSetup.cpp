ad209388a84e83abaffacaa090658c5b9c4c1653
angrysonicgamer <angrysonicgamer@gmail.com>
2024-05-07 19:20:58 +0300

    Implemented automatic character widths calculation

--- Impress/Mod/FontSetup.cpp
+++ Impress/Mod/FontSetup.cpp
@@ -6,7 +6,7 @@
 
 VoidFunc(UnloadFonts, 0x6B62C0);
 VoidFunc(LoadFonts, 0x6B6130);
-
+DataPointer(int*, AsciiSFontBuffer, 0xB5D64C);
 DataArray(byte, MainFontSetup, 0x89F3E8, 224);
 DataArray(byte, ChaoWorldFontSetup, 0x8A78D0, 224);
 
@@ -40,200 +40,30 @@ void LoadFont(const char* modPath, const char* fontPath)
 }
 
 
-// Setting up character widths
+// Automatic character widths calculation
 
-struct LetterData
+void CalculateCharacterWidths()
 {
-	byte LetterCode;
-	byte Width;
-};
-
-std::vector<LetterData> ImpressSetup
-{
-	{ '!', 5 }, // using chars here to visualize
-	{ '"', 8 },
-	{ '#', 15 },
-	{ '$', 12 },
-	{ '%', 18 },
-	{ '&', 17 },
-	{ '\'', 3 },
-	{ '(', 6 },
-	{ ')', 6 },
-	{ '*', 7 },
-	{ '+', 13 },
-	{ ',', 4 },
-	{ '-', 7 },
-	{ '.', 5 },
-	{ '/', 9 },
-	{ '0', 13 },
-	{ '1', 6 },
-	{ '2', 13 },
-	{ '3', 13 },
-	{ '4', 13 },
-	{ '5', 13 },
-	{ '6', 13 },
-	{ '7', 11 },
-	{ '8', 13 },
-	{ '9', 13 },
-	{ ':', 5 },
-	{ ';', 5 },
-	{ '<', 13 },
-	{ '=', 13 },
-	{ '>', 13 },
-	{ '?', 12 },
-	{ '@', 20 },
-	{ 'A', 12 },
-	{ 'B', 11 },
-	{ 'C', 13 },
-	{ 'D', 12 },
-	{ 'E', 10 },
-	{ 'F', 10 },
-	{ 'G', 12 },
-	{ 'H', 12 },
-	{ 'I', 4 },
-	{ 'J', 7 },
-	{ 'K', 13 },
-	{ 'L', 8 },
-	{ 'M', 15 },
-	{ 'N', 11 },
-	{ 'O', 14 },
-	{ 'P', 11 },
-	{ 'Q', 13 },
-	{ 'R', 13 },
-	{ 'S', 13 },
-	{ 'T', 10 },
-	{ 'U', 12 },
-	{ 'V', 13 },
-	{ 'W', 19 },
-	{ 'X', 15 },
-	{ 'Y', 14 },
-	{ 'Z', 12 },
-	{ '[', 6 },
-	{ '\\', 9 },
-	{ ']', 6 },
-	{ '^', 13 },
-	{ '_', 11 },
-	{ '`', 6 },
-	{ 'a', 10 },
-	{ 'b', 11 },
-	{ 'c', 10 },
-	{ 'd', 11 },
-	{ 'e', 10 },
-	{ 'f', 9 },
-	{ 'g', 12 },
-	{ 'h', 10 },
-	{ 'i', 4 },
-	{ 'j', 5 },
-	{ 'k', 10 },
-	{ 'l', 4 },
-	{ 'm', 16 },
-	{ 'n', 9 },
-	{ 'o', 11 },
-	{ 'p', 11 },
-	{ 'q', 11 },
-	{ 'r', 6 },
-	{ 's', 9 },
-	{ 't', 7 },
-	{ 'u', 10 },
-	{ 'v', 10 },
-	{ 'w', 15 },
-	{ 'x', 11 },
-	{ 'y', 11 },
-	{ 'z', 10 },
-	{ '{', 8 },
-	{ '|', 3 },
-	{ '}', 8 },
-	{ '~', 15 },
-
-	{ 0x8A, 13 }, //Š	
-
-	{ 0x8C, 20 }, //Œ
-
-	{ 0x8E, 10 }, //ž
-
-	{ 0x9A, 9 }, //š
-
-	{ 0x9C, 17 }, //œ
-
-	{ 0x9E, 12 }, //Ž
-	{ 0x9F, 14 }, //Ÿ
-
-	{ L'¡', 5 },
-
-	{ L'¿', 12 },
-	{ L'À', 12 },
-	{ L'Á', 12 },
-	{ L'Â', 12 },
-	{ L'Ã', 12 },
-	{ L'Ä', 12 },
-	{ L'Å', 12 },
-	{ L'Æ', 20 },
-	{ L'Ç', 13 },
-	{ L'È', 10 },
-	{ L'É', 10 },
-	{ L'Ê', 10 },
-	{ L'Ë', 10 },
-	{ L'Ì', 6 },
-	{ L'Í', 6 },
-	{ L'Î', 7 },
-	{ L'Ï', 7 },
-	{ L'Ð', 13 },
-	{ L'Ñ', 11 },
-	{ L'Ò', 14 },
-	{ L'Ó', 14 },
-	{ L'Ô', 14 },
-	{ L'Õ', 14 },
-	{ L'Ö', 14 },
-	{ L'×', 12 },
-	{ L'Ø', 14 },
-	{ L'Ù', 12 },
-	{ L'Ú', 12 },
-	{ L'Û', 12 },
-	{ L'Ü', 12 },
-	{ L'Ý', 14 },
-	{ L'Þ', 11 },
-	{ L'ß', 11 },
-
-	{ L'à', 10 },
-	{ L'á', 10 },
-	{ L'â', 10 },
-	{ L'ã', 10 },
-	{ L'ä', 10 },
-	{ L'å', 10 },
-	{ L'æ', 16 },
-	{ L'ç', 10 },
-	{ L'è', 10 },
-	{ L'é', 10 },
-	{ L'ê', 10 },
-	{ L'ë', 10 },
-	{ L'ì', 6 },
-	{ L'í', 6 },
-	{ L'î', 7 },
-	{ L'ï', 7 },
-	{ L'ð', 11 },
-	{ L'ñ', 9 },
-	{ L'ò', 11 },
-	{ L'ó', 11 },
-	{ L'ô', 11 },
-	{ L'õ', 11 },
-	{ L'ö', 11 },
-	{ L'÷', 13 },
-	{ L'ø', 11 },
-	{ L'ù', 10 },
-	{ L'ú', 10 },
-	{ L'û', 10 },
-	{ L'ü', 10 },
-	{ L'ý', 11 },
-	{ L'þ', 11 },
-	{ L'ÿ', 11 },
-};
-
-void WriteFontData(const std::vector<LetterData>& fontData)
-{
-	for (auto& letter : fontData)
+	MainFontSetup[0] = 9; // vanilla width of space
+	ChaoWorldFontSetup[0] = 9;
+	
+	for (int charNumber = 1; charNumber < 224; charNumber++) // starting from 1 because 0 is space (empty pixels)
 	{
-		MainFontSetup[letter.LetterCode - ' '] = letter.Width;
-		ChaoWorldFontSetup[letter.LetterCode - ' '] = letter.Width;
+		std::vector<int> rowWidthValues;
+		rowWidthValues.push_back(0);
+		for (int row = 0; row < 24; row++)
+		{
+			int rowWidth = 0;
+			for (int column = 0; column < 24; column++)
+			{
+				if (AsciiSFontBuffer[charNumber * 24 * 24 + row * 24 + column] != 0)
+					rowWidth = column + 1; // getting the last non-zero value in a row, that would represent its width
+			}
+			if (rowWidth > rowWidthValues.back())
+				rowWidthValues.push_back(rowWidth); // adding that width if it tops a previous width
+		}
+		MainFontSetup[charNumber] = rowWidthValues.back(); // the max width would be the character width we need
+		ChaoWorldFontSetup[charNumber] = rowWidthValues.back();
 	}
 }
 
@@ -241,5 +71,5 @@ void WriteFontData(const std::vector<LetterData>& fontData)
 void SetUpFont(const char* modPath, const char* fontPath)
 {
 	LoadFont(modPath, fontPath);
-	WriteFontData(ImpressSetup);
+	CalculateCharacterWidths();
 }
